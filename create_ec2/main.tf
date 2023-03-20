20b033a7efeadc371fe4309a25a80fae7977bdd7
shankaru98 <124458531+shankaru98@users.noreply.github.com>
2023-03-20 19:40:15 +0530

    Update main.tf

--- create_ec2/main.tf
+++ create_ec2/main.tf
@@ -1,5 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "my_ec2_instance" {
-  ami = var.ami_id
+  ami = data.aws_ami.ubuntu.id
   instance_type = var.ec2_type
   key_name = var.ec2_pem
    vpc_security_group_ids = [var.ec2_sg_id]
